5f3f2e93a89ffb4e46436d8d2fcd536e102e5629
Andrii Cherevatyi <andreycherevat@gmail.com>
2022-12-25 03:34:39 +0200

    added User.cs with initialization and Match.cs with properties

---
+++ Match.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Lab_3
+{
+    class Match
+    {
+    }
+}
---
+++ User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Lab_3
+{
+    class User
+    {
+        private static int UserCounter = 0;
+
+        public static ArrayList<User> UserList = new ArrayList<User>();
+
+        public int ID { get; }
+        public string Name { get; }
+
+        public User(string name)
+        {
+            if (name.Length <= 1) throw new ArgumentException("Username must contain 2 or more characters");
+            UserCounter++;
+            ID = UserCounter;
+            this.Name = name;
+        }
+
+
+    }
+}
